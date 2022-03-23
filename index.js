d830eeeac704a6482498d5fd72977814f8f68c8b
Dylan Temples <dtemps123@gmail.com>
2022-03-23 15:33:23 +0000

    add comments, use bindings

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+// import the native addon using bindings
 // const daqhats_module = require( './build/Release/daqhats-node.node' );
-const daqhats_module = require('bindings')('daqhats-node');
-//exports.daqhats = daqhats_module.setup;
-module.exports = daqhats_module;
+const daqhats_node = require('bindings')('daqhats-node');
+
+// expose the module API
+module.exports = daqhats_node;
 
 // console.log( 'exports : ', mcc118_module); console.log();
-
 // console.log( 'mcc118_module.setup       : ', mcc118_module.setup() ); console.log();
 // console.log( 'mcc118_module.single_read : ', mcc118_module.single_read() ); console.log();
 // console.log( 'mcc118_module.finalize    : ', mcc118_module.finalize() ); console.log();

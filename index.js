cb84dfc657b25c3f4371c768ea7d1369d9a8723c
Dylan Temples <dtemps123@gmail.com>
2022-03-23 15:02:39 +0000

    make it define an entry point

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const mcc118_module = require( './build/Release/daqhats-node.node' );
+// const daqhats_module = require( './build/Release/daqhats-node.node' );
+const daqhats_module = require('bindings')('daqhats-node');
+exports.daqhats = daqhats_module.setup;
 
-console.log( 'exports : ', mcc118_module); console.log();
+// console.log( 'exports : ', mcc118_module); console.log();
 
-console.log( 'mcc118_module.setup       : ', mcc118_module.setup() ); console.log();
-console.log( 'mcc118_module.single_read : ', mcc118_module.single_read() ); console.log();
-console.log( 'mcc118_module.finalize    : ', mcc118_module.finalize() ); console.log();
+// console.log( 'mcc118_module.setup       : ', mcc118_module.setup() ); console.log();
+// console.log( 'mcc118_module.single_read : ', mcc118_module.single_read() ); console.log();
+// console.log( 'mcc118_module.finalize    : ', mcc118_module.finalize() ); console.log();
 
